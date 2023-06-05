5ccd011e2dadb71ddd9ca7033b41956f16010fb2
leonardopadilha <leonardopadilha28@gmail.com>
2023-06-05 05:49:18 -0300

    Início de criação das roles para usuários

---
+++ api/routes/roleRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const RoleController = require('../controllers/roleController');
+
+const router = Router()
+
+router
+    .post('/roles', RoleController.cadastrar)
+    .get('/role')
+    .get('/role/:id')
+    .delete('/role/:id')
+    .put('/role/:id')
+
+module.exports = router
