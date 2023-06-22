e0bf97efbfc9b13fff0d61db71f385a34e1853ee
leonardopadilha <leonardopadilha28@gmail.com>
2023-06-22 07:01:42 -0300

    Preparando o relacionamento entre as tabelas

--- api/models/roles.js
+++ api/models/roles.js
@@ -10,7 +10,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      roles.belongsToMany(models.usuarios, {
+        through: models.usuarios_roles,
+        as: 'roles_do_usuario',
+        foreignKey: 'role_id'
+      })
+
+      roles.belongsTo(models.permissoes, {
+        through: models.roles_permissoes,
+        as: 'roles_das_permissoes',
+        foreignKey: 'roles_id'
+      })
     }
   }
   roles.init({
